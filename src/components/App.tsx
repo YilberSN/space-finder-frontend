d1383577d35fb6fa36ceb0533f60831a80e0c12f
Yilber <yilber.serrano@gmail.com>
2022-02-06 13:28:16 -0500

    Router and navbar

---
+++ src/components/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {User} from '../model/Model';
+import {AuthService} from '../services/AuthService';
+import {Login} from './Login';
+import {Router, Route, Switch } from 'react-router-dom';
+import { history } from '../utils/History';
+import { Navbar } from './Navbar';
+import { Home } from './Home';
+import { Profile } from './Profile';
+
+interface AppState{
+  user: User | undefined
+}
+
+export class App extends React.Component<{}, AppState>{
+
+  private authService: AuthService = new AuthService();
+
+  constructor(props:any){
+    super(props)
+    this.state={
+      user:undefined
+    }
+    this.setUser = this.setUser.bind(this);
+  }
+
+  private setUser(user:User){
+    this.setState({
+      user: user
+    })
+    console.log('Setting the user!: '+user)
+  }
+
+  render(){
+    return (
+      
+      <div className='wrapper'>
+        <Router history={history}>
+        <div>
+            <Navbar user={this.state.user}/>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/login'>
+                <Login authService={this.authService} setUser={this.setUser}/>
+              </Route>
+              <Route exact path='/profile' component={Profile}/>
+            </Switch>
+          </div>
+
+        </Router>
+      </div>
+    )
+  }
+}
+
