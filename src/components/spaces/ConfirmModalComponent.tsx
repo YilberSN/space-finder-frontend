bd12115abb9aaf14fbc7af84c02bf7876c45da27
Yilber <yilber.serrano@gmail.com>
2022-02-06 16:49:44 -0500

    Modal window spaces

---
+++ src/components/spaces/ConfirmModalComponent.tsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import './ConfirModalComponentProps.css';
+
+interface ConfirModalComponentProps{
+    show:boolean,
+    content:string,
+    closeM: () => void
+}
+
+export class ConfirModalComponent extends Component<ConfirModalComponentProps>{
+
+    render(){
+        if(!this.props.show){
+            return null
+        }else{
+            return <div className="modal">
+                <div className="modal-content">
+                    <h2>You tried to reserve and ...</h2>
+                    <h3 className="modalText">{this.props.content}</h3>
+                    <button onClick={()=>this.props.closeM()}>Ok, close</button>
+                </div>
+            </div>
+        }
+    }
+
+}
